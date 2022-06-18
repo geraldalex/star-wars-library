53b4393daded2c32b8eacc1bdbfeabe8080c9cad
geraldalex <alex1989chekan@gmail.com>
2022-06-18 10:59:15 +0300

    gh-pages

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -7,7 +7,7 @@ import styles from "./App.module.css";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/star-wars-library/">
       <div className={styles.wrapper}>
         <Header/>
 
